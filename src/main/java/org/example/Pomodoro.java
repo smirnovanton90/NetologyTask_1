ed8741119d3c1883f599816ecad8ec416cb52f14
smirnovanton90 <smirnovanton90@gmail.com>
2022-09-19 22:38:43 +0300

    Сделана простейшая валидация ввода

--- src/main/java/org/example/Pomodoro.java
+++ src/main/java/org/example/Pomodoro.java
@@ -12,34 +12,66 @@ public class Pomodoro {
 
          */
         System.out.println("Привет! Введи команду");
+        System.out.println("Для получения подсказки введите команду -help");
         //Ввод данных и нарезка
-        String[] cmd = new Scanner(System.in).nextLine().split(" ");
-        /*
-        Логирование нарезанного массива
-        System.out.println(Arrays.toString(cmd));
-        */
 
+        // Валидация наличия необходимых команд
+        boolean validationCheck = false;
+        String[] cmd;
+
+        do {
+            cmd = new Scanner(System.in).nextLine().split(" ");
+            if (!Arrays.asList(cmd).contains("-w")) {
+                System.out.println("Среди команд отсутствует команда -w. Повторите ввод.");
+                continue;
+            }
+            if (!Arrays.asList(cmd).contains("-b")) {
+                System.out.println("Среди команд отсутствует команда -b. Повторите ввод.");
+                continue;
+            }
+            if (!Arrays.asList(cmd).contains("-count")) {
+                System.out.println("Среди команд отсутствует команда -count. Повторите ввод.");
+                continue;
+            }
+            validationCheck = true;
+        }
+        while (!validationCheck);
+
+        int workMin = 25; //Время работы
+        int breakMin = 5; //Время отдыха
+        int count = 1; //Количество подходов
+        boolean isCallHelp = false;
         int i = 0;
+
         for(i=0; i<cmd.length; i++) {
             switch(cmd[i]) {
                 case "-help" -> {
-                    System.out.println("Это Pomodoro!");
+                    isCallHelp = true;
+                    helpMessage();
                 }
-                case "-w" -> {
-                    System.out.println("Это было -w");
-
-                }
-                case "-b" -> {
-                    System.out.println("Это было -b");
-
-                }
-                case "-count" -> {
-                    System.out.println("Это было -count");
-
-                }
+                case "-w" -> workMin = Integer.parseInt(cmd[++i]);
+                case "-b" -> breakMin = Integer.parseInt(cmd[++i]);
+                case "-count" -> count = Integer.parseInt(cmd[++i]);
             }
         }
+        if (!isCallHelp) {
+            System.out.printf("Параметры программы: Работаем %d минут, " +
+                    "отдыхаем %d минут, количество повторений - %d", workMin, breakMin, count);
+        }
+    }
 
-
+    private static void helpMessage() {
+        System.out.println(
+                "\n\nЭто Pomodoro! Сделай свое время более эффективным!\n");
+        System.out.println(
+                "Список команд:\n");
+        System.out.println(
+                "     -help - информация о программе");
+        System.out.println(
+                "     -w - время работы в минутах");
+        System.out.println(
+                "     -b - время отдыха в минутах");
+        System.out.println(
+                "     -count - количество повторов");
     }
 }
